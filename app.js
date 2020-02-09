e812ba020ae82104bb0c214ae19b9b634c737fea
Philip Papurt <ginkoid@gmail.com>
2020-02-09 18:15:05 -0500

    add api framework

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express()
 
 app.use(bodyParser.json())
 
-app.use('/api', require(path.join(__dirname, '/server/api')))
+app.use('/api/v1', require(path.join(__dirname, '/server/api')))
 
 const staticPath = path.join(__dirname, '/static')
 app.use(express.static(staticPath, { extensions: ['html'] }))
---
+++ server/responses/index.js
@@ -0,0 +1,48 @@
+const responseList = {
+  goodLogin: {
+    status: 200,
+    message: 'The login was successful.'
+  },
+  goodRegister: {
+    status: 200,
+    message: 'The account was created.'
+  },
+  badEmail: {
+    status: 400,
+    message: 'The email address is malformed.'
+  },
+  badUnknownEmail: {
+    status: 404,
+    message: 'The account does not exist.'
+  },
+  badKnownEmail: {
+    status: 409,
+    message: 'An account with this email already exists.'
+  },
+  badPassword: {
+    status: 401,
+    message: 'The password is incorrect.'
+  },
+  badBody: {
+    status: 400,
+    message: 'The request body does not meet requirements.'
+  },
+  badToken: {
+    status: 401,
+    message: 'The token provided is invalid.'
+  },
+  errorInternal: {
+    status: 500,
+    message: 'An internal error occurred.'
+  }
+}
+
+const responses = {}
+Object.keys(responseList).forEach((kind) => {
+  responses[kind] = kind
+})
+
+module.exports = {
+  responseList,
+  responses
+}
