6ccacd0b3283414a54f76019ad43fc8d4d41d2b8
Robert <robertchen@live.com>
2020-02-09 08:51:48 +0000

    Added database framework

---
+++ server/database/auth.js
@@ -0,0 +1,13 @@
+
+
+module.exports = {
+  getUser: ({ id }) => {
+
+  },
+  makeUser: ({ id, name, email, division, password }) => {
+
+  },
+  updateUser: ({ id, name, email, division, password }) => {
+
+  }
+}
