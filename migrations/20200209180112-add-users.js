7ec9b0c6e0576f0ad40b6625c1fe754fe5274c21
Robert <robertchen@live.com>
2020-02-09 18:58:39 +0000

    Added migrations

---
+++ migrations/20200209180112-add-users.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(pgm) {
+  pgm.createExtension("uuid-ossp")
+  pgm.createTable("users", {
+    id: { type: "uuid", primaryKey: true, default: pgm.func('uuid_generate_v4()')  },
+    name: { type: "string", unique: true, notNull: true },
+    email: { type: "string", unique: true, notNull: true },
+    password: { type: "string", notNull: true },
+    division: { type: "string", notNull: true }
+  });
+};
+
+exports.down = function(pgm) {
+  pgm.dropTable("users");
+  pgm.dropExtension("uuid-ossp")
+};
+
+exports._meta = {
+  "version": 1
+};
