96ced5d21d5751cb37b0ed77e423e8839f9bdada
Philip Papurt <ginkoid@gmail.com>
2020-02-09 02:50:25 -0500

    add token utils

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const app = require('./app')
 
 const PORT = process.env.PORT || 3000
---
+++ server/auth/token.js
@@ -0,0 +1,57 @@
+const { promisify } = require('util')
+const crypto = require('crypto')
+
+const randomBytes = promisify(crypto.randomBytes)
+const tokenKey = Buffer.from(process.env.APP_TOKEN_KEY, 'base64')
+
+const tokenKinds = {
+  auth: 0
+}
+
+const encryptToken = async (content) => {
+  const iv = await randomBytes(12)
+  const cipher = crypto.createCipheriv('aes-256-gcm', tokenKey, iv)
+  const cipherText = cipher.update(JSON.stringify(content))
+  cipher.final()
+  const tokenContent = Buffer.concat([iv, cipherText, cipher.getAuthTag()])
+  return tokenContent.toString('base64')
+}
+
+const decryptToken = async (token) => {
+  try {
+    const tokenContent = Buffer.from(token, 'base64')
+    const iv = tokenContent.slice(0, 12)
+    const authTag = tokenContent.slice(tokenContent.length - 16)
+    const cipher = crypto.createDecipheriv('aes-256-gcm', tokenKey, iv)
+    cipher.setAuthTag(authTag)
+    const plainText = cipher.update(tokenContent.slice(12, tokenContent.length - 16))
+    return JSON.parse(plainText)
+  } catch (e) {
+    return null
+  }
+}
+
+const getUserId = async (token) => {
+  const content = await decryptToken(token)
+  if (content === null) {
+    return null
+  }
+  const { k: kind, id: userId } = content
+  if (kind !== tokenKinds.auth) {
+    return null
+  }
+  return userId
+}
+
+const getToken = async (userId) => {
+  const token = await encryptToken({
+    k: tokenKinds.auth,
+    id: userId
+  })
+  return token
+}
+
+module.exports = {
+  getUserId,
+  getToken
+}
