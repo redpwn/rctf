74e0474fa06589d8bc1c35c481b8a6849db71f99
Robert <robertchen@live.com>
2020-02-09 19:07:11 +0000

    Switched to pooling

--- server/database/client.js
+++
@@ -1,9 +0,0 @@
-const connectionString = process.env.DATABASE_URL;
-
-const { Client } = require("pg")
-const client = new Client({
-  connectionString
-})
-client.connect()
-
-module.exports = client
