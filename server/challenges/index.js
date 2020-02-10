53e2bc66081fa5d16bab3be243bf3cca8429858e
Robert <robertchen@live.com>
2020-02-10 01:05:40 +0000

    yarn lint

--- server/challenges/index.js
+++ server/challenges/index.js
@@ -1,16 +1,16 @@
 module.exports = {
   getAllChallenges: () => {
     return [{
-      name: "Test",
-      category: "Pwn",
-      author: "Test",
-      description: "A test challenge",
+      name: 'Test',
+      category: 'Pwn',
+      author: 'Test',
+      description: 'A test challenge',
       points: {
-        min: 50
+        min: 50,
         max: 100
       },
-      files: ["static/security.txt"],
-      id: "random-uuid-here"
+      files: ['static/security.txt'],
+      id: 'random-uuid-here'
     }]
   },
   getChallenge: id => {
