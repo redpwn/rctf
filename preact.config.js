2d0ef869f3691be9c9bdfd94b77740f59f86d2b5
Philip Papurt <ginkoid@gmail.com>
2020-02-15 00:13:35 -0500

    expose some env to ejs client renderer

---
+++ preact.config.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const config = require('./config')
+
+export default (webpackConfig, env, helpers) => {
+  if (env.ssr) {
+    return
+  }
+  const { plugin } = helpers.getPluginsByName(webpackConfig, 'HtmlWebpackPlugin')[0]
+  plugin.options.view = {
+    assetsPrefix: env.production ? '/assets' : '',
+    ctfName: config.ctfName
+  }
+}
