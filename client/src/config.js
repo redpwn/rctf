a9e19d50e1d7bf2326e6867da9acca85e43c0d17
Robert Chen <robertchen@live.com>
2020-02-16 23:19:25 -0800

    Added home page

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,5 @@
 export default {
   apiEndpoint: 'http://localhost:3000/api/v1',
-  ctfTitle: ' | redpwnCTF 2020'
+  ctfTitle: ' | redpwnCTF 2020',
+  ctfName: 'redpwnCTF 2020'
 }
---
+++ client/src/pages/home.js
@@ -0,0 +1,35 @@
+import { Component } from 'preact'
+import config from '../config'
+import 'linkstate/polyfill'
+import withStyles from '../components/jss'
+
+export default withStyles({
+  root: {
+    padding: '25px'
+  },
+  row: {
+    marginBottom: '50px'
+  },
+  logo: {
+    height: '150px',
+    width: '150px'
+  }
+}, class Home extends Component {
+  componentDidMount () {
+    document.title = 'Home' + config.ctfTitle
+  }
+
+  render ({ classes }) {
+    return (
+      <div class={classes.root}>
+        <div class={`${classes.row} row u-center`}>
+          <h2 class='level'>{config.ctfName}</h2>
+        </div>
+        <div class={`${classes.row} row u-center`}>
+          <img src='https://redpwn.net/logo.png' class={`avatar ${classes.logo}`} />
+        </div>
+
+      </div>
+    )
+  }
+})
