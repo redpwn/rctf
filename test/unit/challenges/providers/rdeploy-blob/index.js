7fa73667f776e00e8910ef94d58ac58bc35263d1
Ethan Wu <ethanwu10@gmail.com>
2020-03-20 13:09:05 -0400

    fix(server): ensure rDeploy challenge data loads in reasonable time
    Add a timeout to the test for loading rDeploy challenge data to fail the
    test if it takes too long (currently >1000ms)

--- test/unit/challenges/providers/rdeploy-blob/index.js
+++ test/unit/challenges/providers/rdeploy-blob/index.js
@@ -4,12 +4,14 @@ const RDeployBlobProvider = require('../../../../../server/providers/challenges/
 
 test('get all challenges', async t => {
   let provider
-  const data = await new Promise((resolve, reject) => {
+  const data = await Promise.race([new Promise((resolve, reject) => {
     provider = new RDeployBlobProvider({
       onUpdate: resolve
       // use default options
     })
-  })
+  }), new Promise((resolve, reject) =>
+    setTimeout(() => reject(new Error('Timed out')), 1000)
+  )])
 
   t.true(Array.isArray(data))
 
