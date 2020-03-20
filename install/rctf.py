5613f135de1f87b67f0357d53539392d1cd7e047
Aaron Esau <git@aaronesau.com>
2020-03-20 15:02:08 -0700

    feat(install): improve logging (colors)

--- install/rctf.py
+++ install/rctf.py
@@ -3,21 +3,84 @@
 
 import sys, os, io, subprocess
 import argparse
-import logging
+import logging, traceback
 
 
-logging.basicConfig(format = '%(message)s', level = logging.DEBUG)
+# custom color logging
+
+
+colored = lambda s, attrs = [] : ''.join([{
+    'lightgray' : '\033[37m',
+    'darkgray' : '\033[90m',
+    'gray' : '\033[2m',
+    'blue' : '\033[34m',
+    'cyan' : '\033[36m',
+    'darkorange' : '\033[33m',
+    'darkred' : '\033[31m',
+    'lightred' : '\033[91m',
+    'bold_white' : '\033[1;37m',
+
+    'bg_red' : '\033[41m',
+
+    'italics' : '\033[3m',
+    'bold' : '\033[01m',
+    'underline' : '\033[04m',
+}[x] for x in attrs]) + s + '\033[0m'
+
+
+class ColorLog(object):
+
+    colormap = {
+        'debug' : ['italics', 'gray'],
+        'info' : ['blue'],
+        'warn' : ['bold', 'darkorange'],
+        'warning' : ['bold', 'darkorange'],
+        'error' : ['bold', 'lightred'],
+        'critical' : ['bg_red', 'bold_white'],
+        'fatal' : ['bg_red', 'bold_white']
+    }
+
+    prompt = {
+        'debug' : ' *  {msg}',
+        'info' : '[*] {msg}',
+        'warn' : '[!] {msg}',
+        'warning' : '[!] {msg}',
+        'error' : '[-] {msg}',
+        'critical' : '[-] {msg}',
+        'fatal' : '[-] {msg}'
+    }
+
+
+    def __init__(self, logger):
+        self._log = logger
+
+
+    def __getattr__(self, name):
+        if name in ['debug', 'info', 'warn', 'warning', 'error', 'critical', 'fatal']:
+            return lambda s, *args, **kwargs: getattr(self._log, name)(
+                colored(ColorLog.prompt[name].format(msg = s), attrs = ColorLog.colormap[name])
+                + ('\n' + colored(traceback.format_exc().strip(), ['italics', 'darkred']) if kwargs.get('exc_info') else '')
+            )
+
+        return getattr(self._log, name)
+
+
+LOG_LEVEL = logging.DEBUG
+
+
+log = ColorLog(logging.getLogger(__name__))
+log.setLevel(LOG_LEVEL)
+stdout = logging.StreamHandler()
+log.addHandler(stdout)
+stdout.setLevel(LOG_LEVEL)
+logging = log # XXX: find a cleaner solution plz
 
 
 # define useful functions
 
 
 def verify_privileges(euid = 0):
-    if os.geteuid() != euid:
-        logging.fatal('This command must be run as root')
-        return False
-
-    return True
+    return os.geteuid() == euid
 
 
 def check_file(fn):
@@ -29,14 +92,17 @@ def execute(command):
     logging.debug('Executing `%s`...' % command)
     
     # shell=False if list, shell=True if str
+    logging.debug('-'*80)
     status_code = subprocess.call(command, shell = isinstance(command, str))
+    logging.debug('-'*80)
 
     if status_code:
-        logging.warning('Command failed to execute; exited with status code %d.' % status_code)
+        logging.error('Command failed to execute; exited with status code %d.' % status_code)
 
     # XXX: check to make sure this status code isn't just docker?
     if status_code == 1 and not verify_privileges(): # permission denied
         logging.warning('Possible permission denied error? Try running as root.\n\n    %s\n' % ' '.join(sys.argv))
+        raise PermissionError('Permission denied. Try running as root.')
 
 
     return status_code == 0
@@ -134,19 +200,37 @@ if __name__ == '__main__':
 
 
     if subcommand in ['start', 'up']:
-        if not rctf.up():
+        logging.info('Starting rCTF...')
+
+        try:
+            rctf.up()
+            logging.info('Started rCTF.')
+        except:
+            logging.fatal('Failed to start rCTF', exc_info = True)
             exit(1)
     elif subcommand in ['stop', 'down']:
-        if not rctf.down():
+        logging.info('Stopping rCTF...')
+
+        try:
+            rctf.down()
+            logging.info('Stopped rCTF.')
+        except:
+            logging.fatal('Failed to stop rCTF', exc_info = True)
             exit(1)
     elif subcommand in ['update', 'upgrade']:
-        if not rctf.upgrade():
-            exit(1)
+        logging.info('Upgrading rCTF instance...')
 
-        logging.info('Successfully updated instance. Run `%s up` to start instance' % sys.argv[0])
-        logging.info('Upgrading %s CLI tool...' % sys.argv[0])
-        
-        # XXX: possible argument injection probably not worth fixing
-        if not (execute(['cp', 'install/rctf.py', sys.argv[0]]) and execute(['chmod', '+x', sys.argv[0]])):
-            logging.fatal('Failed to upgrade rCTF CLI tool')
+        try:
+            rctf.upgrade()
+    
+            logging.info('Successfully updated instance.')
+            logging.info('Upgrading %s CLI tool...' % sys.argv[0])
+            
+            # XXX: possible argument injection probably not worth fixing
+            execute(['cp', 'install/rctf.py', sys.argv[0]])
+            execute(['chmod', '+x', sys.argv[0]])
+
+            logging.info('Finished. Run `%s up` to start the rCTF instance again.' % sys.argv[0])
+        except:
+            logging.fatal('Failed to upgrade rCTF', exc_info = True)
             exit(1)
