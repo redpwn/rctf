5b8c3e1e4d3e0c187e0ffd63ae5c772d9acafbf6
Ethan Wu <ethanwu10@gmail.com>
2020-03-21 15:04:28 -0400

    style: refactor eslint config
    Add back root eslintrc enforcing standard style and other project-wide
    options, make server/ eslintrc add typescript plugins only, and move ava
    plugin to test/.eslintrc.js
    
    Also drop unused global definitions

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,22 +1,5 @@
 module.exports = {
   parser:  '@typescript-eslint/parser',
-  env: {
-    es6: true
-  },
-  extends: [
-    'standard',
-    'plugin:ava/recommended'
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
-  },
-  parserOptions: {
-    ecmaFeatures: {
-    },
-    ecmaVersion: 2018,
-    sourceType: 'module'
-  },
   plugins: [
     "typescript"
   ],
