15a93e717dfcf1ecdbb2e3a9c2ea626b74c5f2c5
Ethan Wu <ethanwu10@gmail.com>
2020-03-21 18:52:49 -0400

    refactor(client): change scoreboard to use hooks
    Refactor Scoreboard component to be a function component using hooks
    instead of a class component. Also add stub functionality for pagination
    (the `setPage` variable is unused, causing lint errors) - everything
    except the actual pagination UI is completed.

--- client/src/pages/scoreboard.js
+++ client/src/pages/scoreboard.js
@@ -1,81 +1,84 @@
-import { Component } from 'preact'
+import { useState, useEffect, useCallback } from 'preact/hooks'
 import config from '../../../config/client'
-import 'linkstate/polyfill'
 import withStyles from '../components/jss'
 
 import { getScoreboard } from '../api/scoreboard'
 
-export default withStyles({
+const TEAMS_PER_PAGE = 100
+
+const Scoreboard = withStyles({
   frame: {
     paddingBottom: '10px'
   }
-}, class Scoreboard extends Component {
-  state = {
-    scores: [],
-    division: 0
-  }
+}, ({ classes }) => {
+  const [scores, setScores] = useState([])
+  const [division, setDivision] = useState('')
+  const [page, setPage] = useState(0)
 
-  componentDidMount () {
-    document.title = `Scoreboard${config.ctfTitle}`
+  const divisionChangeHandler = useCallback((e) => setDivision(e.target.value), [setDivision])
 
-    getScoreboard()
+  useEffect(() => { document.title = `Scoreboard${config.ctfTitle}` }, [])
+  useEffect(() => {
+    const _division = division === '' ? undefined : division
+    console.log(_division)
+    getScoreboard({
+      division: _division,
+      offset: page * TEAMS_PER_PAGE,
+      limit: TEAMS_PER_PAGE
+    })
       .then(data => {
-        this.setState({
-          scores: data.leaderboard
-        })
+        console.log(data)
+        setScores(data.leaderboard)
       })
-  }
+  }, [division, page])
 
-  render ({ classes }, { scores, division }) {
-    return (
-      <div class='row u-center' style='align-items: initial !important'>
-        <div class='col-3'>
-          <div class={`frame ${classes.frame}`}>
-            <div class='frame__body'>
-              <div class='frame__title title'>Config</div>
-              <div class='input-control'>
-                <select required class='select' name='division' value={division} onChange={this.linkState('division')}>
-                  <option value='' disabled selected>Division</option>
-                  <option value='0'>High School</option>
-                  <option value='1'>College</option>
-                  <option value='2'>Other</option>
-                </select>
-              </div>
+  return (
+    <div class='row u-center' style='align-items: initial !important'>
+      <div class='col-3'>
+        <div class={`frame ${classes.frame}`}>
+          <div class='frame__body'>
+            <div class='frame__title title'>Config</div>
+            <div class='input-control'>
+              <select required class='select' name='division' value={division} onChange={divisionChangeHandler}>
+                <option value='' selected>All</option>
+                <option value='0'>High School</option>
+                <option value='1'>College</option>
+                <option value='2'>Other</option>
+              </select>
             </div>
           </div>
         </div>
-        <div class='col-6'>
-          <div class={`frame ${classes.frame}`} style='padding-top: 25px'>
-            <div class='frame__body'>
-              <table class='table small'>
-                <thead>
-                  <tr>
-                    <th style='width: 10px'>#</th>
-                    <th>Team</th>
-                    <th style='width: 50px'>Points</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {
-                    scores.map(({ id, name, score }, idx) =>
-                      <tr key={id}>
-                        <td>{idx + 1}</td>
-                        <td>
-                          <a href={`/profile/${id}`}>{name}</a>
-                        </td>
-                        <td>{score}</td>
-                      </tr>
-                    )
-                  }
-                </tbody>
-              </table>
-
-            </div>
-          </div>
-
-        </div>
-
       </div>
-    )
-  }
+      <div class='col-6'>
+        <div class={`frame ${classes.frame}`} style='padding-top: 25px'>
+          <div class='frame__body'>
+            <table class='table small'>
+              <thead>
+                <tr>
+                  <th style='width: 10px'>#</th>
+                  <th>Team</th>
+                  <th style='width: 50px'>Points</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  scores.map(({ id, name, score }, idx) =>
+                    <tr key={id}>
+                      <td>{idx + 1}</td>
+                      <td>
+                        <a href={`/profile/${id}`}>{name}</a>
+                      </td>
+                      <td>{score}</td>
+                    </tr>
+                  )
+                }
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 })
+
+export default Scoreboard
