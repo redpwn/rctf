78bd3ad4c9c629fdec59f524bf3cb41c2cf70a85
Ethan Wu <ethanwu10@gmail.com>
2020-03-29 23:38:25 -0400

    feat(client): make logout button a modal dialog
    The logout route has been replaced with a logout button that displays a
    modal confirming logout.
    
    Refactorings: Header made to use preact-router's `Match` element to
    detect when a link is active. This removes the need for `App` to keep
    track of the current path.
    
    BUGS: due to `App` no longer updating on route changes, the header items
    do not update when the user logs in or out.

---
+++ client/src/components/logout-button.js
@@ -0,0 +1,55 @@
+import { Fragment } from 'preact'
+import { useState, useCallback } from 'preact/hooks'
+import Modal from './modal'
+import { logout } from '../api/auth'
+
+function LogoutDialog ({ onClose, ...props }) {
+  const wrappedOnClose = useCallback(e => {
+    e.preventDefault()
+    onClose()
+  }, [onClose])
+  const doLogout = useCallback(e => {
+    e.preventDefault()
+    logout()
+    onClose()
+  }, [onClose])
+
+  return (
+    <Modal {...props}>
+      <div class='modal-header'>
+        <div class='modal-title'>Logout</div>
+      </div>
+      <div class='modal-body'>
+        <div>Make sure you have saved your team code, or you may not be able to login again!</div>
+      </div>
+      <div class='modal-footer'>
+        <div class='btn-container u-inline-block'>
+          <button class='btn-small' onClick={wrappedOnClose}>Cancel</button>
+        </div>
+        <div class='btn-container u-inline-block'>
+          <button class='btn-small btn-danger outline' onClick={doLogout}>Logout</button>
+        </div>
+      </div>
+    </Modal>
+  )
+}
+
+function LogoutButton ({ ...props }) {
+  const [isDialogVisible, setIsDialogVisible] = useState(false)
+  const onClick = useCallback(e => {
+    e.preventDefault()
+    setIsDialogVisible(true)
+  }, [])
+  const onClose = useCallback(() => setIsDialogVisible(false), [])
+
+  return (
+    <Fragment>
+      <a {...props} href='#' native onClick={onClick}>
+        Logout
+      </a>
+      <LogoutDialog open={isDialogVisible} onClose={onClose} />
+    </Fragment>
+  )
+}
+
+export default LogoutButton
--- client/src/routes/logout.js
+++
@@ -1,8 +0,0 @@
-import { useLayoutEffect } from 'preact/hooks'
-import { logout } from '../api/auth'
-import 'linkstate/polyfill'
-
-export default () => {
-  useLayoutEffect(logout)
-  return null
-}
