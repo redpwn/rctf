ded4768503cd0c872841a1d8ae5df619a746529a
Ethan Wu <ethanwu10@gmail.com>
2020-03-30 19:05:36 -0400

    style(client): enforce single quotes in jsx

--- client/src/components/icon.js
+++ client/src/components/icon.js
@@ -1,6 +1,6 @@
 export default function Icon ({ glyph, viewBox, ...props }) {
   return (
-    <svg {...props} viewBox={viewBox} xmlns="http://www.w3.org/2000/svg">
+    <svg {...props} viewBox={viewBox} xmlns='http://www.w3.org/2000/svg'>
       <use xlinkHref={`#${glyph}`} />
     </svg>
   )
