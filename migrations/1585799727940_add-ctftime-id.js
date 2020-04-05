27f3271ffad76eec68de21f1d7debd7d10aafd77
Philip Papurt <ginkoid@gmail.com>
2020-04-05 03:42:04 -0400

    feat(api): make sure either email or ctftime auth is enabled

--- migrations/1585799727940_add-ctftime-id.js
+++ migrations/1585799727940_add-ctftime-id.js
@@ -3,9 +3,11 @@ exports.up = function (pgm) {
     ctftime_id: { type: 'string', unique: true, notNull: false }
   })
   pgm.alterColumn('users', 'email', { notNull: false })
+  pgm.addConstraint('users', 'require_email_or_ctftime_id', 'check ((email is not null) or (ctftime_id is not null))')
 }
 
 exports.down = function (pgm) {
-  pgm.dropColumns('solves', ['ctftime_id'])
+  pgm.dropColumns('users', ['ctftime_id'])
   pgm.alterColumn('users', 'email', { notNull: true })
+  pgm.dropConstraint('users', 'require_email_or_ctftime_id')
 }
