54324a311898bdbbca26ea1643841c08092b4ac7
Philip Papurt <ginkoid@gmail.com>
2020-04-09 20:45:45 -0400

    refactor(api): remove empty graph fix

--- server/cache/leaderboard.js
+++ server/cache/leaderboard.js
@@ -183,9 +183,6 @@ const setGraph = async ({ leaderboards }) => {
       values.push(sample + ':' + score[0], score[1])
     })
   })
-  if (values.length === 0) {
-    return
-  }
   await redisEvalsha(
     await setGraphScript,
     2,
