4a37b07366ea2e3a76f4b309535ef416301cebe7
Philip Papurt <ginkoid@gmail.com>
2020-05-19 02:32:54 -0400

    refactor(client): make client api helpers use async/await

--- client/src/api/admin/challs.js
+++ client/src/api/admin/challs.js
@@ -1,16 +1,13 @@
 import { request } from '../util'
 
-export const getChallenges = () => {
-  return request('GET', '/admin/challs')
-    .then(resp => resp.data)
+export const getChallenges = async () => {
+  return (await request('GET', '/admin/challs')).data
 }
 
-export const updateChallenge = ({ id, data }) => {
-  return request('PUT', `/admin/challs/${encodeURIComponent(id)}`, { data })
-    .then(resp => resp.data)
+export const updateChallenge = async ({ id, data }) => {
+  return (await request('PUT', `/admin/challs/${encodeURIComponent(id)}`, { data })).data
 }
 
-export const deleteChallenge = ({ id }) => {
-  return request('DELETE', `/admin/challs/${encodeURIComponent(id)}`)
-    .then(resp => resp.data)
+export const deleteChallenge = async ({ id }) => {
+  return (await request('DELETE', `/admin/challs/${encodeURIComponent(id)}`)).data
 }
