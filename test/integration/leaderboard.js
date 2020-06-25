63a04ba55b27f42de3eae0e2733df6f435b39d46
Philip Papurt <ginkoid@gmail.com>
2020-06-25 17:30:04 -0400

    fix(api): remove ctftime leaderboard test

--- test/integration/leaderboard.js
+++ test/integration/leaderboard.js
@@ -18,13 +18,3 @@ test('succeeds with goodLeaderboard', async t => {
   t.is(resp.body.kind, 'goodLeaderboard')
   t.true(Array.isArray(resp.body.data.leaderboard))
 })
-
-test('ctftime integration succeeds with goodCtftimeLeaderboard', async t => {
-  const resp = await request(app.server)
-    .get(process.env.API_ENDPOINT + '/integrations/ctftime/leaderboard')
-    .query({ limit: 1, offset: 0 })
-    .expect('Content-Type', /json/)
-    .expect(responseList.goodCtftimeLeaderboard.status)
-
-  t.true(Array.isArray(resp.body.standings))
-})
