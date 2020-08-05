e66b4d725945aed802b4e1f0111fbfcdc6c11a78
Ethan Wu <ethanwu10@gmail.com>
2020-08-04 22:36:59 -0400

    refactor(server): type auth token handling
    The kinds of auth tokens are made into an enum, and the data for each
    kind is also specified. Methods for making and decoding tokens will
    correctly infer the data type based on the kind provided.

--- server/auth/token.ts
+++ server/auth/token.ts
@@ -1,15 +1,49 @@
 import { promisify } from 'util'
 import crypto from 'crypto'
 import config from '../config/server'
+import { User } from '../database/users'
 
 const randomBytes = promisify(crypto.randomBytes)
 const tokenKey = Buffer.from(config.tokenKey, 'base64')
 
-export const tokenKinds = {
-  auth: 0,
-  team: 1,
-  verify: 2,
-  ctftimeAuth: 4
+export enum tokenKinds {
+  auth = 0,
+  team = 1,
+  verify = 2,
+  ctftimeAuth = 4
+}
+
+export enum VerifyTokenKinds {
+  update = 'update',
+  register = 'register'
+}
+
+export type AuthTokenData = string
+export type TeamTokenData = string
+export interface VerifyTokenData {
+  verifyId: string
+  kind: VerifyTokenKinds
+  userId: User['id']
+  email: User['email']
+  division: User['division']
+}
+export type CtftimeAuthTokenData = string
+
+// Internal interface of type definitions for typing purposes only
+// - this interface does not describe a real data-structure
+interface TokenDataTypes {
+  [tokenKinds.auth]: AuthTokenData;
+  [tokenKinds.team]: TeamTokenData;
+  [tokenKinds.verify]: VerifyTokenData;
+  [tokenKinds.ctftimeAuth]: CtftimeAuthTokenData;
+}
+
+export type Token = string
+
+interface InternalTokenData<Kind extends tokenKinds> {
+  k: Kind
+  t: number
+  d: TokenDataTypes[Kind]
 }
 
 const tokenExpiries = {
@@ -21,7 +55,7 @@ const tokenExpiries = {
 
 const timeNow = () => Math.floor(Date.now() / 1000)
 
-const encryptToken = async (content) => {
+const encryptToken = async (content: unknown): Promise<string> => {
   const iv = await randomBytes(12)
   const cipher = crypto.createCipheriv('aes-256-gcm', tokenKey, iv)
   const cipherText = cipher.update(JSON.stringify(content))
@@ -30,7 +64,7 @@ const encryptToken = async (content) => {
   return tokenContent.toString('base64')
 }
 
-const decryptToken = async (token) => {
+const decryptToken = async (token: string): Promise<unknown | null> => {
   try {
     const tokenContent = Buffer.from(token, 'base64')
     const iv = tokenContent.slice(0, 12)
@@ -39,14 +73,14 @@ const decryptToken = async (token) => {
     cipher.setAuthTag(authTag)
     const plainText = cipher.update(tokenContent.slice(12, tokenContent.length - 16))
     cipher.final()
-    return JSON.parse(plainText)
+    return JSON.parse(plainText.toString())
   } catch (e) {
     return null
   }
 }
 
-export const getData = async (expectedTokenKind, token) => {
-  const content = await decryptToken(token)
+export const getData = async <Kind extends tokenKinds>(expectedTokenKind: Kind, token: Token): Promise<TokenDataTypes[Kind] | null> => {
+  const content = await decryptToken(token) as InternalTokenData<Kind> | null
   if (content === null) {
     return null
   }
@@ -60,11 +94,11 @@ export const getData = async (expectedTokenKind, token) => {
   return data
 }
 
-export const getToken = async (tokenKind, data) => {
+export const getToken = async <Kind extends tokenKinds>(tokenKind: Kind, data: TokenDataTypes[Kind]): Promise<Token> => {
   const token = await encryptToken({
     k: tokenKind,
     t: timeNow(),
     d: data
-  })
+  }) as Token
   return token
 }
