3ec9d47328fec4b19812c3bb1ffcfa130eb43cc9
Ethan Wu <ethanwu10@gmail.com>
2021-03-27 20:59:59 -0700

    fix(client): update Card for theme-ui 0.6
    Update tests to test when running theme-ui both with and without CSS
    variables, update Card to use functional form of ThemeProvider theme and
    also update `rawColors` accordingly when theme-ui is running with CSS
    variables.

--- packages/client/src/components/card.test.tsx
+++ packages/client/src/components/card.test.tsx
@@ -1,76 +1,124 @@
 import '@testing-library/jest-dom/extend-expect'
+import { matchers } from '@emotion/jest'
 
 import { render } from '@testing-library/preact'
 
 import Card from './card'
 
-import { FunctionComponent } from 'react'
-import { useThemeUI, ThemeProvider } from 'theme-ui'
+import { useThemeUI, ThemeProvider, Theme } from 'theme-ui'
 import theme from '../util/theme'
 
-const ThemeColorDisplayer: FunctionComponent<{
-  color: string
-}> = ({ color, ...props }) => {
+expect.extend(matchers)
+
+const ThemeColorDisplayer = ({
+  color,
+  ...props
+}: { color: string } & Omit<JSX.IntrinsicElements['div'], 'sx'>) => {
   const { theme } = useThemeUI()
 
-  return <div {...props}>{theme.colors?.[color] ?? 'undefined'}</div>
+  return (
+    <div {...props} sx={{ color }}>
+      {theme.rawColors?.[color] ?? 'undefined'}
+    </div>
+  )
 }
 
-test('default background color is cardBackground', () => {
-  const { queryByTestId } = render(
+const runBgColorTest = ({
+  themeColors,
+  cardBg,
+  expectedColor,
+}: {
+  themeColors: Theme['colors']
+  cardBg?: string
+  expectedColor: string
+}) => {
+  const run = (
+    useCustomProperties: boolean,
+    doExpect: (e: HTMLElement) => void
+  ) => {
+    const { getByTestId } = render(
+      <ThemeProvider
+        theme={{
+          ...theme,
+          useCustomProperties,
+          colors: themeColors,
+        }}
+      >
+        <Card bg={cardBg}>
+          <ThemeColorDisplayer
+            color='background'
+            data-testid='colordisplayer'
+          />
+        </Card>
+      </ThemeProvider>
+    )
+
+    doExpect(getByTestId('colordisplayer'))
+  }
+
+  test('with CSS variables', () =>
+    run(true, e => expect(e).toHaveTextContent(expectedColor)))
+  test('without CSS variables', () =>
+    run(false, e => expect(e).toHaveStyleRule('color', expectedColor)))
+}
+
+describe('default background color is cardBackground', () => {
+  runBgColorTest({
+    themeColors: {
+      background: 'badcolor',
+      cardBackground: 'testcolor1',
+    },
+    expectedColor: 'testcolor1',
+  })
+})
+
+describe('background color respects theme', () => {
+  runBgColorTest({
+    themeColors: {
+      background: 'badcolor',
+      custom: 'testcolor2',
+    },
+    cardBg: 'custom',
+    expectedColor: 'testcolor2',
+  })
+})
+
+describe('background color not in theme should be passed through', () => {
+  runBgColorTest({
+    themeColors: {
+      background: 'badcolor',
+    },
+    cardBg: 'testcolor3',
+    expectedColor: 'testcolor3',
+  })
+})
+
+test('misc theme properties are kept', () => {
+  const space = [0, 4, 8, 16, 32, 64]
+
+  const SpaceDumper = (props: JSX.IntrinsicElements['div']) => {
+    const { theme } = useThemeUI()
+
+    return <div {...props}>{JSON.stringify(theme.space)}</div>
+  }
+
+  const { getByTestId } = render(
     <ThemeProvider
       theme={{
         ...theme,
-        colors: {
-          background: 'badcolor',
-          cardBackground: 'testcolor1',
-        },
+        space,
       }}
     >
       <Card>
-        <ThemeColorDisplayer color='background' data-testid='colordisplayer' />
+        <SpaceDumper data-testid='spacedumper' />
       </Card>
     </ThemeProvider>
   )
 
-  expect(queryByTestId('colordisplayer')).toHaveTextContent('testcolor1')
+  expect(getByTestId('spacedumper')).toHaveTextContent(JSON.stringify(space))
 })
 
-test('background color respects theme', () => {
-  const { queryByTestId } = render(
-    <ThemeProvider
-      theme={{
-        ...theme,
-        colors: {
-          background: 'badcolor',
-          custom: 'testcolor2',
-        },
-      }}
-    >
-      <Card bg='custom'>
-        <ThemeColorDisplayer color='background' data-testid='colordisplayer' />
-      </Card>
-    </ThemeProvider>
-  )
-
-  expect(queryByTestId('colordisplayer')).toHaveTextContent('testcolor2')
-})
-
-test('background color not in theme should be passed through', () => {
-  const { queryByTestId } = render(
-    <ThemeProvider
-      theme={{
-        ...theme,
-        colors: {
-          background: 'badcolor',
-        },
-      }}
-    >
-      <Card bg='testcolor3'>
-        <ThemeColorDisplayer color='background' data-testid='colordisplayer' />
-      </Card>
-    </ThemeProvider>
-  )
-
-  expect(queryByTestId('colordisplayer')).toHaveTextContent('testcolor3')
+test('does not die when outside of ThemeProvider', () => {
+  const { queryByTestId } = render(<Card data-testid='card' />)
+  expect(queryByTestId('card')).toBeInTheDocument()
 })
