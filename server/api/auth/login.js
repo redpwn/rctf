d3915549225550f7c1ad95331359b936ae460dcf
Darin Mao <maodarin@gmail.com>
2021-08-04 23:36:58 -0400

    feat: update ionData on login

--- server/api/auth/login.js
+++ server/api/auth/login.js
@@ -1,6 +1,7 @@
 import * as database from '../../database'
 import * as auth from '../../auth'
 import { responses } from '../../responses'
+import { deepEquals } from '../../util'
 
 export default {
   method: 'POST',
@@ -26,12 +27,14 @@ export default {
   },
   handler: async ({ req }) => {
     let user
+    let ionData
     if (req.body.ionToken !== undefined) {
       const ionToken = await auth.token.getData(auth.token.tokenKinds.ionAuth, req.body.ionToken)
       if (ionToken === null) {
         return responses.badIonToken
       }
-      const { ionId } = ionToken
+      let ionId
+      ({ ionId, ionData } = ionToken)
       user = await database.users.getUserByIonId({ ionId })
     } else {
       const uuid = await auth.token.getData(auth.token.tokenKinds.team, req.body.teamToken)
@@ -42,6 +45,9 @@ export default {
     }
     if (user === undefined) {
       return responses.badUnknownUser
+    }
+    if (!deepEquals(ionData, user.ionData)) {
+      await database.users.updateUser({ id: user.id, ionData })
     }
     const authToken = await auth.token.getToken(auth.token.tokenKinds.auth, user.id)
     return [responses.goodLogin, {
